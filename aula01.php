6497fa4084426e1a294c4c1ea05957d54c8a6321
Joaquim de Campos <kinmagicu@outlook.pt>
2021-06-30 17:48:12 +0100

    Teste

--- aula01.php
+++ aula01.php
@@ -1,3 +1,13 @@
 <?php 
         echo "Olá Mundo";
+        $teste="Olá mundo";
+        $t=1 ;
+        $l=true 
+        if ($l = true) {
+                echo $l 
+                else{
+                        echo false
+                }
+        }
+
 ?>
